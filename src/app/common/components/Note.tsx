177e3f19be7f4949aea5bc65acede22a1e41bcc7
Ivane <gdzelishviliv@gmail.com>
2024-09-09 15:11:54 +0400

    added toastify messeges on input

--- src/app/common/components/Note.tsx
+++ src/app/common/components/Note.tsx
@@ -8,6 +8,8 @@ import {
   loadTasksFromLocalStorage,
   saveTasksToLocalStorage,
 } from "../utils/UtilityFunctions";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface NoteProps {
   color: string;
@@ -33,10 +35,18 @@ const Note: React.FC<NoteProps> = ({ color, noteId, index, onRemove }) => {
   }, [tasks, noteId]);
 
   const addTask = () => {
-    if (inputText.trim()) {
+    if (inputText === "") {
+      toast.error("Type something!!!", {
+        position: 'bottom-left',
+        autoClose: 3000,
+      });
+    } else if (inputText.trim()) {
       const newTask = { id: generateId(), text: inputText, completed: false };
       setTasks((prevTasks) => [...prevTasks, newTask]);
       setInputText("");
+      toast.success("Success Notification !", {
+        position: "bottom-left",
+      });
     }
   };
 
@@ -67,71 +77,74 @@ const Note: React.FC<NoteProps> = ({ color, noteId, index, onRemove }) => {
   };
 
   return (
-    <Draggable draggableId={noteId} index={index}>
-      {(provided) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          className={`relative flex flex-col-reverse p-4 m-4 border rounded-lg shadow-lg ${color}`}
-        >
-          <div className="space-y-2 mb-12">
-            <div className="flex space-x-2 p-2 justify-end">
-              <button
-                onClick={removeNote}
-                className="text-red-500 bg-white p-2 rounded-full"
-              >
-                ❌
-              </button>
-              <button
-                onClick={downloadNote}
-                className="text-white bg-blue-500 p-2 rounded-full"
-              >
-                📥
-              </button>
-            </div>
-            <div className="flex gap-2 mb-4">
-              <input
-                type="text"
-                value={inputText}
-                onKeyDown={handleKeyDown}
-                placeholder="Type task"
-                onChange={(e) => setInputText(e.target.value)}
-                className="mt-2 w-full p-2 border rounded h-10"
-              />
-              <button
-                onClick={addTask}
-                className="mt-2 bg-green-500 text-white p-2 rounded h-10 w-24"
-              >
-                Add
-              </button>
-            </div>
-            {tasks.map((task) => (
-              <div
-                key={task.id}
-                className="flex items-center justify-between gap-8"
-              >
-                <input
-                  type="checkbox"
-                  checked={task.completed}
-                  onChange={() => toggleTaskCompletion(task.id)}
-                  className="form-checkbox"
-                />
-                <span className={task.completed ? "line-through" : ""}>
-                  {task.text}
-                </span>
+    <>
+      <Draggable draggableId={noteId} index={index}>
+        {(provided) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className={`relative flex flex-col-reverse p-4 m-4 border rounded-lg shadow-lg ${color}`}
+          >
+            <div className="space-y-2 mb-12">
+              <div className="flex space-x-2 p-2 justify-end">
                 <button
-                  onClick={() => deleteTask(task.id)}
-                  className="text-red-500"
+                  onClick={removeNote}
+                  className="text-red-500 bg-white p-2 rounded-full"
                 >
                   ❌
                 </button>
+                <button
+                  onClick={downloadNote}
+                  className="text-white bg-blue-500 p-2 rounded-full"
+                >
+                  📥
+                </button>
               </div>
-            ))}
+              <div className="flex gap-2 mb-4">
+                <input
+                  type="text"
+                  value={inputText}
+                  onKeyDown={handleKeyDown}
+                  placeholder="Type task"
+                  onChange={(e) => setInputText(e.target.value)}
+                  className="mt-2 w-full p-2 border rounded h-10"
+                />
+                <button
+                  onClick={addTask}
+                  className="mt-2 bg-green-500 text-white p-2 rounded h-10 w-24"
+                >
+                  Add
+                </button>
+              </div>
+              {tasks.map((task) => (
+                <div
+                  key={task.id}
+                  className="flex items-center justify-between gap-8"
+                >
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => toggleTaskCompletion(task.id)}
+                    className="form-checkbox"
+                  />
+                  <span className={task.completed ? "line-through" : ""}>
+                    {task.text}
+                  </span>
+                  <button
+                    onClick={() => deleteTask(task.id)}
+                    className="text-red-500"
+                  >
+                    ❌
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </Draggable>
+        )}
+      </Draggable>
+      <ToastContainer />
+    </>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-import './globals.css';
-import { ReactNode } from 'react';
-import { MainLayout } from './common/components/_organisms/layouts/MainLayout';
+import "./globals.css";
+import { ReactNode } from "react";
+import { MainLayout } from "./common/components/_organisms/layouts/MainLayout";
+import { ToastContainer } from "react-toastify";
 
 export const metadata = {
-  title: 'Notes',
-  description: 'Note application',
+  title: "Notes",
+  description: "Note application",
 };
 
 const RootLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <MainLayout>
       {children}
+      <ToastContainer />
     </MainLayout>
   );
 };
